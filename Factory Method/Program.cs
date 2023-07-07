b459a9c6d7dec44bdb8ac7c205a3c3a9c3607c3d
Piersilvio Spicoli <88087303+Piersilvio@users.noreply.github.com>
2023-07-07 11:22:22 +0200

    Add files via upload

---
+++ Factory Method/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Factory_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factory method!\n");
+            new Client().Main();
+        }
+    }
+
+    class Client
+    {
+        public void Main()
+        {
+            Console.WriteLine("App: Launched with the ConcreteCreator1.");
+            ClientCode(new ConcreteCreator1());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("App: Launched with the ConcreteCreator2.");
+            ClientCode(new ConcreteCreator2());
+        }
+
+        public void ClientCode(Creator creator)
+        {
+            Console.WriteLine("Client: I'm not aware of the creator's class," +
+                "but it still works.\n" + creator.SomeOperation());
+        }
+    }
+}
